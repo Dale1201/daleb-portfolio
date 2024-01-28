00afcf389ca9888d3afa6e2b3306b4834bf8da38
Dale Beniga <dalecd9@gmail.com>
2024-01-28 21:27:42 +1100

    fix projects allignment

--- daleb-portfolio/src/components/sections/Projects.vue
+++ daleb-portfolio/src/components/sections/Projects.vue
@@ -33,62 +33,70 @@ function incrementCarousel() {
 <template>
   <section id="projects">
     <h1>Projects</h1>
-    <h2>Mr Bluesky - Weather Booking App</h2>
-    <div class="info-badge-container">
-      <InfoBadge> 2023 </InfoBadge>
-      <InfoBadge> React </InfoBadge>
-      <InfoBadge> Typescript </InfoBadge>
-      <InfoBadge> HTML/CSS </InfoBadge>
-      <WebsiteLogo
-        url="https://github.com/chessworld/weather-booking-client"
-        icon="fa-brands fa-github"
-        class="github-logo"
-      />
-    </div>
+    <div class="projects-content">
+      <div>
+        <h2>Mr Bluesky - Weather Booking App</h2>
+        <div class="info-badge-container">
+          <InfoBadge> 2023 </InfoBadge>
+          <InfoBadge> React </InfoBadge>
+          <InfoBadge> Typescript </InfoBadge>
+          <InfoBadge> HTML/CSS </InfoBadge>
+          <WebsiteLogo
+            url="https://github.com/chessworld/weather-booking-client"
+            icon="fa-brands fa-github"
+            class="github-logo"
+          />
+        </div>
 
-    <div class="booking-app-content">
-      <div style="display: flex; flex-direction: column; align-items: center">
-        <div class="logo-container">
-          <img src="/weather-booking-app/mr_bluesky_logo.png" alt="Mr Bluesky" />
-        </div>
-        <p class="booking-blurb">
-          Have a special occasion coming up? <br />
-          Need a certain weather...? <br />
-          Then book it!
-        </p>
-        <b class="booking-blurb special-text">Introducing Mr Bluesky {{ "&nbsp;".repeat(18) }} </b>
-      </div>
-      <div style="display: flex; justify-content: center; min-height: 28rem">
-        <div class="img-container" v-if="false">
-          <img src="/weather-booking-app/wba-home.png" alt="Mr Bluesky" />
-        </div>
-        <div style="display: flex; flex-direction: column">
-          <div class="img-container">
-            <img :src="carouselItems[carouselIndex]" alt="Mr Bluesky" />
+        <div class="booking-app-content">
+          <div style="display: flex; flex-direction: column; align-items: center">
+            <div class="logo-container">
+              <img src="/weather-booking-app/mr_bluesky_logo.png" alt="Mr Bluesky" />
+            </div>
+            <p class="booking-blurb">
+              Have a special occasion coming up? <br />
+              Need a certain weather...? <br />
+              Then book it!
+            </p>
+            <b class="booking-blurb special-text">Introducing Mr Bluesky {{ "&nbsp;".repeat(18) }} </b>
           </div>
-          <div class="carousel__dots">
-            <span
-              v-for="(item, index) in carouselItems"
-              :key="index"
-              :class="{ 'dot-active': carouselIndex === index }"
-              @click="carouselIndex = index"
-            ></span>
+          <div style="display: flex; justify-content: center; min-height: 28rem">
+            <div class="img-container" v-if="false">
+              <img src="/weather-booking-app/wba-home.png" alt="Mr Bluesky" />
+            </div>
+            <div style="display: flex; flex-direction: column">
+              <div class="img-container">
+                <img :src="carouselItems[carouselIndex]" alt="Mr Bluesky" />
+              </div>
+              <div class="carousel__dots">
+                <span
+                  v-for="(item, index) in carouselItems"
+                  :key="index"
+                  :class="{ 'dot-active': carouselIndex === index }"
+                  @click="carouselIndex = index"
+                ></span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    <div style="margin: 5rem 0 8rem 0;">
-      <h2>Youtube Clone - WIP</h2>
-      <div class="info-badge-container">
-        <InfoBadge> 2024 </InfoBadge>
-        <InfoBadge> React - Next.js </InfoBadge>
-        <InfoBadge> Nodejs </InfoBadge>
-        <InfoBadge> Firebase </InfoBadge>
-        <WebsiteLogo url="https://github.com/Dale1201/youtube-clone" icon="fa-brands fa-github" class="github-logo" />
-      </div>
-      <div style="margin-top: 2rem">
-        <p>A simple webapp with that will allow users to upload and view their videos.</p>
-        <p>React front-end and Nodejs backend with Firestore as the database</p>
+        <div style="margin: 5rem 0 8rem 0">
+          <h2>Youtube Clone - WIP</h2>
+          <div class="info-badge-container">
+            <InfoBadge> 2024 </InfoBadge>
+            <InfoBadge> React - Next.js </InfoBadge>
+            <InfoBadge> Nodejs </InfoBadge>
+            <InfoBadge> Firebase </InfoBadge>
+            <WebsiteLogo
+              url="https://github.com/Dale1201/youtube-clone"
+              icon="fa-brands fa-github"
+              class="github-logo"
+            />
+          </div>
+          <div style="margin-top: 2rem">
+            <p>A simple webapp with that will allow users to upload and view their videos.</p>
+            <p>React front-end and Nodejs backend with Firestore as the database</p>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -99,7 +107,18 @@ section {
   border-bottom: 1px solid $dark-text;
   min-height: 40rem;
   padding: 0 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
+
+.projects-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 80rem;
+}
+
 h1 {
   @include heading;
   text-align: center;
