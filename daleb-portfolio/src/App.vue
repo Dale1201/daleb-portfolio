ed44d0527f59bfd7a2369ebe90e57b44e25a1bfa
Dale Beniga <dalecd9@gmail.com>
2024-04-30 17:57:19 +1000

    add main header animation

--- daleb-portfolio/src/App.vue
+++ daleb-portfolio/src/App.vue
@@ -5,12 +5,25 @@ import Experience from "./components/sections/Experience.vue";
 import Projects from "./components/sections/Projects.vue";
 import Techstack from "./components/sections/Techstack.vue";
 import AboutMe from "./components/sections/AboutMe.vue";
+import gsap from "gsap";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  gsap.from(".header", {
+    duration: 1,
+    y: -100,
+    opacity: 0,
+    ease: "ease-in",
+  });
+})
+
+
 </script>
 
 <template>
-  <MainHeader />
+  <MainHeader class="header" />
   <Home />
-  <AboutMe />
+  <!-- <AboutMe /> -->
   <Experience />
   <Projects />
   <Techstack />
