f858a3b3c314fea8fd5e6cd3724d755d85c12dd9
Dale Beniga <dalecd9@gmail.com>
2024-05-04 22:54:06 +1000

    start project refactor

--- daleb-portfolio/src/components/sections/Projects.vue
+++ daleb-portfolio/src/components/sections/Projects.vue
@@ -13,6 +13,33 @@ onMounted(() => {
 onUnmounted(() => {
   clearInterval(interval);
 });
+
+// Projects
+const projects = ref([
+  {
+    name: "NBA Archives",
+    year: 2024,
+    tech: ["Vue.js", "Django", "Web Scraping"],
+    github: "https://github.com/Dale1201/nba-dashboard",
+    image: "/projects/nba-archives.PNG",
+    website: "https://nba-archives.live",
+  },
+  {
+    name: "Mr Bluesky",
+    year: 2023,
+    tech: ["Mobile", "React", "Typescript", "HTML/CSS"],
+    github: "https://github.com/chessworld/weather-booking-client",
+    image: "/projects/mr-bluesky.PNG",
+  },
+  {
+    name: "Youtube Clone",
+    year: 2024,
+    tech: ["React - Next.js", "Nodejs", "Firebase"],
+    github: "https://github.com/Dale1201/youtube-clone",
+    image: "/projects/mr_bluesky_logo.png",
+  },
+]);
+
 // Carousel logic
 const carouselIndex = ref(0);
 const carouselItems = ref([
@@ -34,6 +61,126 @@ function incrementCarousel() {
   <section id="projects">
     <h1>Projects</h1>
     <div class="projects-content">
+      <div class="card" v-for="project in projects">
+        <div>
+          <h2>{{ project.name }}</h2>
+          <div class="info-badge-container">
+            <InfoBadge v-for="tech in project.tech" :key="tech">{{ tech }}</InfoBadge>
+          </div>
+          <WebsiteLogo :url="project.github" icon="fa-brands fa-github" class="github-logo" />
+        </div>
+        <div class="img-container">
+          <img :src="project.image" alt="Project image" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+section {
+  border-bottom: 1px solid var(--clr-border);
+  min-height: 40rem;
+}
+
+.projects-content {
+}
+
+.card {
+  border: 1px solid var(--clr-border);
+  border-radius: 12px;
+  margin: 1rem 0;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: fit-content;
+  margin-inline: auto;
+
+  .img-container {
+    margin-inline: auto;
+    border-radius: 0 0 6px 6px;
+    max-width: 20rem;
+    overflow: hidden;
+  }
+}
+
+h1 {
+  @include heading;
+  padding-left: 1rem;
+}
+
+h2 {
+  font-weight: normal;
+}
+
+.info-badge-container {
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+}
+
+.github-logo {
+  display: inline-block;
+  width: 2rem;
+}
+
+.booking-app-content {
+  display: grid;
+  gap: 2rem;
+  padding: 2rem 0;
+  @media (min-width: 50rem) {
+    grid-template-columns: 1fr 1fr;
+    justify-items: center;
+  }
+}
+
+.logo-container {
+  height: fit-content;
+}
+.booking-blurb {
+  text-align: justify;
+  display: flex;
+  justify-content: center;
+}
+
+.special-text {
+  @include special-text;
+}
+
+.img-container {
+  max-width: 14rem;
+  padding: 1rem;
+  @media (min-width: 40rem) {
+    padding: 0;
+  }
+}
+
+.link-header {
+  a {
+    text-decoration: none;
+  }
+}
+
+.carousel__dots {
+  display: flex;
+  justify-content: center;
+  gap: 0.5rem;
+  margin-top: 1rem;
+
+  span {
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    background-color: var(--clr-text);
+    cursor: pointer;
+  }
+  .dot-active {
+    background-color: var(--clr-primary);
+  }
+}
+</style>
+
+<!-- <div class="projects-content">
       <div>
         <h2>Mr Bluesky - Weather Booking App</h2>
         <div class="info-badge-container">
@@ -124,98 +271,4 @@ function incrementCarousel() {
           </div>
         </div>
       </div>
-    </div>
-  </section>
-</template>
-
-<style scoped lang="scss">
-section {
-  border-bottom: 1px solid var(--clr-border);
-  min-height: 40rem;
-  padding: 0 2rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.projects-content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  max-width: 80rem;
-}
-
-h1 {
-  @include heading;
-}
-
-h2 {
-  font-weight: normal;
-}
-
-.info-badge-container {
-  display: flex;
-  gap: 1rem;
-  align-items: center;
-}
-
-.github-logo {
-  display: inline-block;
-  width: 2rem;
-}
-
-.booking-app-content {
-  display: grid;
-  gap: 2rem;
-  padding: 2rem 0;
-  @media (min-width: 50rem) {
-    grid-template-columns: 1fr 1fr;
-    justify-items: center;
-  }
-}
-
-.logo-container {
-  height: fit-content;
-}
-.booking-blurb {
-  text-align: justify;
-  display: flex;
-  justify-content: center;
-}
-
-.special-text {
-  @include special-text;
-}
-
-.img-container {
-  max-width: 14rem;
-  padding: 1rem;
-  @media (min-width: 40rem) {
-    padding: 0;
-  }
-}
-
-.link-header {
-  a {
-    text-decoration: none;
-  }
-}
-
-.carousel__dots {
-  display: flex;
-  justify-content: center;
-  gap: 0.5rem;
-  margin-top: 1rem;
-
-  span {
-    width: 0.5rem;
-    height: 0.5rem;
-    border-radius: 50%;
-    background-color: var(--clr-text);
-    cursor: pointer;
-  }
-  .dot-active {
-    background-color: var(--clr-primary);
-  }
-}
-</style>
+    </div> -->
